80903225c661324b5c9f0891bd689011e1ccc416
llouis <llouis0622@gmail.com>
2024-01-09 15:55:36 +0900

    .

---
+++ java-start/src/cond/ex/ExchangeRateEx.java
@@ -0,0 +1,16 @@
+package cond.ex;
+
+public class ExchangeRateEx {
+
+    public static void main(String[] args) {
+        int doller = 10;
+
+        if (doller < 0) {
+            System.out.println("잘못된 금액입니다.");
+        } else if (doller == 0) {
+            System.out.println("환전할 금액이 없습니다.");
+        } else {
+            System.out.println("환전 금액은 " + 1300 * doller + "원입니다.");
+        }
+    }
+}
