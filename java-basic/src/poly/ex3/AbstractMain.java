aa0ae26b9807896bb799c3f29949eee9510c756f
LLouis <llouis0622@gmail.com>
2024-01-24 20:34:55 +0900

    .

---
+++ java-basic/src/poly/ex3/AbstractMain.java
@@ -0,0 +1,23 @@
+package poly.ex3;
+
+public class AbstractMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+
+        cat.sound();
+        cat.move();
+
+        soundAnimal(cat);
+        soundAnimal(dog);
+        soundAnimal(cow);
+    }
+
+    private static void soundAnimal(AbstractAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
