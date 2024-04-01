e60d826c2f62c9ee77f56a6ab84a90a1ffadfd96
LLouis <llouis0622@gmail.com>
2024-04-02 00:48:04 +0900

    .

---
+++ java-mid1/src/lang/object/equals/UserV2.java
@@ -0,0 +1,24 @@
+package lang.object.equals;
+
+public class UserV2 {
+    private String id;
+
+    public UserV2(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        UserV2 user = (UserV2) obj;
+
+        return id.equals(user.id);
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        User user = (User) o;
+//        return Objects.equals(id, user.id);
+//    }
+}
