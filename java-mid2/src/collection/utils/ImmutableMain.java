3a5ca47a778614a943f06ee35de8b890cfd19703
LLouis <llouis0622@gmail.com>
2024-05-16 00:37:36 +0900

    .

---
+++ java-mid2/src/collection/utils/ImmutableMain.java
@@ -0,0 +1,17 @@
+package collection.utils;
+
+import java.util.*;
+
+public class ImmutableMain {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3);
+
+        ArrayList<Integer> mutableList = new ArrayList<>(list);
+        mutableList.add(4);
+        System.out.println("mutableList = " + mutableList);
+        System.out.println("mutableList class = " + mutableList.getClass());
+
+        List<Integer> unmodifiableList = Collections.unmodifiableList(mutableList);
+        System.out.println("unmodifiableList class = " + unmodifiableList.getClass());
+    }
+}
